d4eb0134c81963e6a577084fa7bf95fb857288bd
Kristopher Howard <khoward82@gmail.com>
2026-06-10 20:32:15 -0400

    Add break and continue to loops script

--- loops.py
+++ loops.py
@@ -1,12 +1,19 @@
-#! usr/bin/env python3
+#!/usr/bin/env python3
+# This program demonstrates while loops, break, and continue
 
-# This program counts from 1-5, then stops.
-
-start = int(input("Enter a number starting number: "))
+start = int(input("Enter a starting number: "))
 x = start
-while x < 6:
-	print(x)
-	x = x + 1
-if start >= 6:
-	print("Please enter a number less than 6 next time.")
 
+while x < 6:
+    if x == 3:
+        print("Skipping 3 with continue")
+        x = x + 1
+        continue
+    if x == 5:
+        print("Hit 5, breaking loop")
+        break
+    print(x)
+    x = x + 1
+
+if start >= 6:
+    print("Please enter a number less than 6 next time.")
