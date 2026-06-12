7bf5b9ed0aae7a5ca1021b1dd24e973e38f8e0de
Kristopher Howard <khoward82@gmail.com>
2026-06-11 20:05:24 -0400

    Add for loops script with f-string device scanner

---
+++ for_loops.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# This script goes through a list of devices.
+
+devices = ["router", "switch", "firewall", "server"]
+
+for device in devices:
+
+	print(f"Scanning device: {device}")
+
+
